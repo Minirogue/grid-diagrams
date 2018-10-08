bcf46b20f513a7d7c6d633aaba62817dcfd52d33
slwitte <ettiwsl@gmail.com>
2018-10-08 16:48:20 -0700

    change compile to be compatible with Crick

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-javac -d bin -cp bin src/GridDiagram.java
-javac -d bin -cp bin src/GridAlgorithm.java
-javac -d bin -cp bin src/Energy.java
-javac -d bin -cp bin src/WangLandau.java
-javac -d bin -cp bin src/RunWangLandau.java
+javac -source 1.8 -target 1.8 -d bin -cp bin src/GridDiagram.java
+javac -source 1.8 -target 1.8 -d bin -cp bin src/GridAlgorithm.java
+javac -source 1.8 -target 1.8 -d bin -cp bin src/Energy.java
+javac -source 1.8 -target 1.8 -d bin -cp bin src/WangLandau.java
+javac -source 1.8 -target 1.8 -d bin -cp bin src/RunWangLandau.java
