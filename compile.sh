fc889808cdbfcf22e12cbc233edd3a3ecb498062
slwitte <ettiwsl@gmail.com>
2018-10-16 17:56:56 -0700

    update compile.sh to compile all pertinent binaries

--- compile.sh
+++ compile.sh
@@ -6,3 +6,5 @@ javac -source 1.8 -target 1.8 -d bin -cp bin src/GridAlgorithm.java
 javac -source 1.8 -target 1.8 -d bin -cp bin src/Energy.java
 javac -source 1.8 -target 1.8 -d bin -cp bin src/WangLandau.java
 javac -source 1.8 -target 1.8 -d bin -cp bin src/RunWangLandau.java
+javac -source 1.8 -target 1.8 -d bin -cp bin src/AnalyzeGrids.java
+javac -source 1.8 -target 1.8 -d bin -cp bin src/testingCode.java
