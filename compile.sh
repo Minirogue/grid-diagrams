04b90cebe122399c9caac14448c2afa6742215f4
slwitte <ettiwsl@gmail.com>
2018-11-06 18:08:03 -0800

    add compilation code for ExtendWeights

--- compile.sh
+++ compile.sh
@@ -8,3 +8,4 @@ javac -source 1.8 -target 1.8 -d bin -cp bin src/WangLandau.java
 javac -source 1.8 -target 1.8 -d bin -cp bin src/RunWangLandau.java
 javac -source 1.8 -target 1.8 -d bin -cp bin src/AnalyzeGrids.java
 javac -source 1.8 -target 1.8 -d bin -cp bin src/testingCode.java
+javac -source 1.8 -target 1.8 -d bin -cp bin src/ExtendWeights.java
