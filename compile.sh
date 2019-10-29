53f822f7435c281936041592fdcdb1e3c3b13887
slwitte <ettiwsl@gmail.com>
2019-10-29 13:14:42 -0700

    update compilation instructions

--- compile.sh
+++ compile.sh
@@ -2,10 +2,11 @@
 
 
 javac -source 1.8 -target 1.8 -d bin -cp bin src/GridDiagram.java
-javac -source 1.8 -target 1.8 -d bin -cp bin src/GridAlgorithm.java
+javac -source 1.8 -target 1.8 -d bin -cp bin -Xlint:deprecation src/GridAlgorithm.java
 javac -source 1.8 -target 1.8 -d bin -cp bin src/Energy.java
-javac -source 1.8 -target 1.8 -d bin -cp bin src/WangLandau.java
-javac -source 1.8 -target 1.8 -d bin -cp bin src/RunWangLandau.java
+javac -source 1.8 -target 1.8 -d bin -cp bin -Xlint:deprecation src/WangLandau.java
+javac -source 1.8 -target 1.8 -d bin -cp bin src/WangLandauT.java
+javac -source 1.8 -target 1.8 -d bin -cp bin -Xlint:deprecation src/RunWangLandau.java
 javac -source 1.8 -target 1.8 -d bin -cp bin src/AnalyzeGrids.java
 javac -source 1.8 -target 1.8 -d bin -cp bin src/testingCode.java
 javac -source 1.8 -target 1.8 -d bin -cp bin src/ExtendWeights.java
